683804b243b05910222b26bee13edadefe02f469
Mohian Dipta <mohianmostofa@gmail.com>
2021-06-15 13:33:35 +0600

    component specifying children, React.fragment

---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    render() {
+        return <React.Fragment>
+            <h1>Hello World!</h1>
+            <button>Increment</button>
+        </React.Fragment>
+    }
+}
+
+export default Counter;
