7c3f45f999215bb1f511463cc9f408794880a348
Mohian Dipta <mohianmostofa@gmail.com>
2021-06-18 13:17:29 +0600

    ...

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+    state = {
+        count: 0,
+        imageURL: 'https://picsum.photos/200/'
+    };
     render() {
-        return <React.Fragment>
-            <h1>Hello World!</h1>
-            <button>Increment</button>
-        </React.Fragment>
+        return (
+            <React.Fragment>
+                <Badge variant="primary">
+                    <span style={{ color: "DodgerBlue" }} >{this.formatCount()}</span>
+                </Badge>
+                <button>Increment</button>
+            </React.Fragment >
+
+        );
     }
+
+
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? "Zero" : count;
+    };
 }
+
 
 export default Counter;
